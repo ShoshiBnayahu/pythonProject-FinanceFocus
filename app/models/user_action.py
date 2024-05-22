6320735b6d79f580059cd8af7df1b3032d884b7b
Shoshi <s0556722535@gmail.com>
2024-05-22 18:17:25 +0300

    Adding `@classmethod` above the validation functions ensures that they operate as class methods rather than instance methods

--- app/models/user_action.py
+++ app/models/user_action.py
@@ -1,22 +1,26 @@
-from _datetime import datetime
-from pydantic import BaseModel,field_validator
+from datetime import datetime
+from pydantic import BaseModel, Field, field_validator
+
 
 class User_Action(BaseModel):
-     id:int
-     user_id:int
-     type:str
-     amount:int
-     datetime:datetime
+    id: int
+    user_id: int
+    type: str
+    amount: int
+    datetime: datetime
 
-     @field_validator('type')
-     def check_type(self, type):
-         """Validates that the type is either 'revenue' or 'expense'"""
-         if type not in ['revenue','expense']:
-            raise ValueError('error,type must to be revenue or expense')
-         return type
-     @field_validator('amount','user_id')
-     def check_positiveNumber(self, num):
+    @field_validator('type')
+    @classmethod
+    def check_type(cls, type):
+        """Validates that the type is either 'revenue' or 'expense'"""
+        if type not in ['revenue', 'expense']:
+            raise ValueError('error, type must be revenue or expense')
+        return type
+
+    @field_validator('amount', 'user_id')
+    @classmethod
+    def check_positiveNumber(cls, num):
         # Ensures that the amount and user_id are positive numbers
         if num < 0:
-            raise ValueError('error, negative number! ')
+            raise ValueError('error, negative number!')
         return num
--- tests/user_test.py
+++ tests/user_test.py
@@ -1,21 +1,53 @@
-# import
-# import pytest
-#
-#
-# def test_login():
-#     assert  user_router.login({
-#       "id": 0,
-#       "name": "Shoshi",
-#       "password": "Sh123456"
-#     })=={
-#       "id": 0,
-#       "name": "Shoshi",
-#       "password": "Sh123456"
-#     }
-#     assert user_router.login({
-#         "id": 0,
-#         "name": "blabla",
-#         "password": "blabla"
-#     }) == {
-#       "detail": "Invalid credentials"
-#     }
+import asyncio
+import pytest
+from app.models.user import User
+from app.services import user_service
+
+"""This module defines unit tests for user-related functionalities in the app."""
+
+@pytest.mark.parametrize("user,expected_id, expected_name, expected_password", [
+    (User(id=3, name='u', password='12345'), 3, 'u', '12345'),
+    (User(id=0, name='shoshi', password='1234'), 0, 'shoshi', '1234'),
+])
+@pytest.mark.asyncio
+async def test_login(user, expected_id, expected_name, expected_password):
+
+    """Tests the login functionality.
+    Uses parameters of a specific user and the expected output of user details.
+    Checks that the output of the login function matches the expected user details.
+    """
+    result = await user_service.login(user)
+    assert result.id == expected_id
+    assert result.name == expected_name
+    assert result.password == expected_password
+
+@pytest.mark.asyncio
+async def test_sign_up():
+
+    """Tests the sign-up functionality for a new user.
+    Creates a new user and checks that the user was successfully registered and received a unique identifier.
+    """
+    new_user = User(id=9, name='michalile', password='123456')
+    result = await user_service.sign_up(new_user)
+    assert result == new_user
+    assert result.id is not None
+
+@pytest.mark.asyncio
+async def test_update_user_detail():
+
+    """Checks the functionality of updating user details.
+    Creates a user with new details and verifies that the details were successfully updated.
+    """
+    updated_user = User(id=6, name='shoshana', password='1234')
+    result = await user_service.update_user_detail(updated_user)
+    assert result == updated_user
+
+
+@pytest.mark.asyncio
+async def test_get_user_by_id():
+    """Checks the functionality of retrieving a user by ID.
+       Verifies that the user found by the given ID matches the expected user.
+    """
+    user = User(id=6, name='shoshana', password='1234')
+    result = await user_service.get_user_by_id(6)
+    assert result == user
