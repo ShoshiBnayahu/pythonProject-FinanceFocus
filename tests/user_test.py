642c9e7af1419c7a8590fae30ff8279f9367539d
Shoshi <s0556722535@gmail.com>
2024-05-22 18:43:02 +0300

    Add async tests for user service

--- tests/user_test.py
+++ tests/user_test.py
@@ -1,53 +1,39 @@
-import asyncio
+# import asyncio
 import pytest
 from app.models.user import User
 from app.services import user_service
 
 """This module defines unit tests for user-related functionalities in the app."""
 
-@pytest.mark.parametrize("user,expected_id, expected_name, expected_password", [
-    (User(id=3, name='u', password='12345'), 3, 'u', '12345'),
-    (User(id=0, name='shoshi', password='1234'), 0, 'shoshi', '1234'),
-])
+@pytest.mark.parametrize("user", [User(id=0, name='michalile', password='123456')])
 @pytest.mark.asyncio
-async def test_login(user, expected_id, expected_name, expected_password):
-
-    """Tests the login functionality.
-    Uses parameters of a specific user and the expected output of user details.
-    Checks that the output of the login function matches the expected user details.
+@pytest.mark.order(2)
+async def test_login(user):
+    """Tests the login functionality for an existing user.
+    Attempts to log in a user and checks that the user was successfully authenticated.
     """
     result = await user_service.login(user)
-    assert result.id == expected_id
-    assert result.name == expected_name
-    assert result.password == expected_password
+    print(result)
+    assert result == user
 
 @pytest.mark.asyncio
+@pytest.mark.order(1)
 async def test_sign_up():
-
     """Tests the sign-up functionality for a new user.
     Creates a new user and checks that the user was successfully registered and received a unique identifier.
     """
-    new_user = User(id=9, name='michalile', password='123456')
+    new_user = User(id=0, name='michalile', password='123456')
     result = await user_service.sign_up(new_user)
-    assert result == new_user
     assert result.id is not None
+    assert result.name == new_user.name
+    assert result.password == new_user.password
 
 @pytest.mark.asyncio
-async def test_update_user_detail():
-
-    """Checks the functionality of updating user details.
-    Creates a user with new details and verifies that the details were successfully updated.
-    """
-    updated_user = User(id=6, name='shoshana', password='1234')
-    result = await user_service.update_user_detail(updated_user)
-    assert result == updated_user
-
-
-@pytest.mark.asyncio
+@pytest.mark.order(3)
 async def test_get_user_by_id():
     """Checks the functionality of retrieving a user by ID.
        Verifies that the user found by the given ID matches the expected user.
     """
-    user = User(id=6, name='shoshana', password='1234')
-    result = await user_service.get_user_by_id(6)
+    user = User(id=0, name='michalile', password='123456')
+    result = await user_service.get_user_by_id(0)
     assert result == user
